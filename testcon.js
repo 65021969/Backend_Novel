8c91e20bd1d70b9d11a19d1631a9ddb99f582f03
Nattawut <nwathnmala@gmail.com>
2025-02-06 21:16:11 +0700

    backend

--- testcon.js
+++ testcon.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const { Client } = require('pg');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = process.env.PORT || 8080; // ใช้พอร์ตที่ Render กำหนดให้
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL, // ใช้ env variable เพื่อความปลอดภัย
@@ -16,6 +23,16 @@ client.connect()
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.get("/novel", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM public.userinfo ORDER BY user_id ASC");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 app.listen(port, () => {
